8a161185db51be29dc9fe61db3b2e72fc026cfce
Nagesh <nagesh.k@tecnolynx.com>
2020-02-20 19:08:42 +0530

    Updates

---
+++ precache-manifest.66f930408d986a9be6ce9b58a100f51e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a468b3669b30493402fcbb4c6a1299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720954c947457fb3c0cf",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d19e84536d3cb51d8359",
+    "url": "/static/css/main.1d6e8fa3.chunk.css"
+  },
+  {
+    "revision": "720954c947457fb3c0cf",
+    "url": "/static/js/2.08f0a44f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.08f0a44f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19e84536d3cb51d8359",
+    "url": "/static/js/main.6454d8b1.chunk.js"
+  },
+  {
+    "revision": "84a18177dfc26901f18b",
+    "url": "/static/js/runtime-main.156c4efa.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/static/media/twitter_icon.512536cb.png"
+  }
+]);
